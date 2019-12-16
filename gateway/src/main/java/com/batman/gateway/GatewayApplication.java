1f02f4f73a9b7be62fa3d8edc6ff034c8b8db74d
qfx <qinfuxiang>
2019-12-16 18:24:10 +0800

    add java config

--- gateway/src/main/java/com/batman/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/batman/gateway/GatewayApplication.java
@@ -1,10 +1,14 @@
 package com.batman.gateway;
 
 import com.batman.gateway.ratelimit.SystemRedisRateLimiter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -18,10 +22,30 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class GatewayApplication {
+    @Autowired
+    private SystemRedisRateLimiter systemRedisRateLimiter;
+
+    @Autowired
+    private RequestRateLimiterGatewayFilterFactory requestRateLimiterGatewayFilterFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("rateLimit_route", p -> p
+//                        .order(0)
+//                        .path("/rateLimit/**")
+//                        .uri("http://localhost:8088/hello")
+//                        .filter(requestRateLimiterGatewayFilterFactory.apply(config -> {
+//                            config.setRateLimiter(systemRedisRateLimiter);
+//                            config.setKeyResolver(this.sysKeyResolver());
+//                            config.setRouteId("rateLimit_route");
+//                        })))
+//                .build();
+//    }
 
     @Bean
     KeyResolver sysKeyResolver() {
--- provide/src/main/java/com/batman/provide/ProvideApplication.java
+++ provide/src/main/java/com/batman/provide/ProvideApplication.java
@@ -3,10 +3,11 @@ package com.batman.provide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/hello")
+@RestController
 public class ProvideApplication {
 
     public static void main(String[] args) {
@@ -14,12 +15,12 @@ public class ProvideApplication {
     }
 
     @GetMapping("/rateLimit")
-    public String hello(){
+    public String hello() {
         return "hello ";
     }
 
     @GetMapping("/rateLimit/hello")
-    public String limit(){
+    public String limit() {
         return "limit ";
     }
 }
